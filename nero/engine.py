cae64da3f803d9061b16f07a46f158280e26f7f5
David <onianidavid@gmail.com>
2018-08-09 15:05:53 -0500

    Private variables

--- nero/engine.py
+++ nero/engine.py
@@ -12,36 +12,36 @@ class Core:
     
     def __init__(self, filename):
         csv.register_dialect('nero', delimiter=',', lineterminator='\n')
-        self.filename = filename
+        self._filename = filename
 
-        file = open(self.filename, 'r')
-        self.task_reader = csv.reader(file, 'nero')
+        file = open(self._filename, 'r')
+        self._task_reader = csv.reader(file, 'nero')
 
-        self.titles = [task[0] for task in self.task_reader]
+        self._titles = [task[0] for task in self._task_reader]
         file.seek(0)
-        self.deadlines = [task[1] for task in self.task_reader]
+        self._deadlines = [task[1] for task in self._task_reader]
 
         file.close()
 
     def __len__(self):
-        return len(self.titles)
+        return len(self._titles)
 
     def __getitem__(self, idx):
-        if idx > len(self.titles) - 1:
+        if idx > len(self._titles) - 1:
             raise IndexError("Index is out of bounds")
         elif idx < 0:
             raise IndexError("Index cannot be less than 0")
 
-        return self.titles[idx]
+        return self._titles[idx]
 
     def __iter__(self):
-        for i in range(len(self.titles)):
-            yield self.titles[i]
+        for i in range(len(self._titles)):
+            yield self._titles[i]
 
     def __str__(self):
         try:
-            max_ttl_len = len(max(self.titles, key=len))
-            max_ddl_len = len(max(self.deadlines, key=len))
+            max_ttl_len = len(max(self._titles, key=len))
+            max_ddl_len = len(max(self._deadlines, key=len))
         except:
             max_ttl_len = max_ddl_len = 0
 
@@ -52,7 +52,7 @@ class Core:
         else:
             res += '\n' + '=' * 25 + '\n'
 
-        for idx, task in enumerate(zip(self.titles, self.deadlines)):
+        for idx, task in enumerate(zip(self._titles, self._deadlines)):
             res += str(idx) + '.' + ''.rjust(3 - len(str(idx)))
             res += ''.join(word.ljust(max_ttl_len + 13) for word in task)
             res += '\n'
@@ -61,31 +61,31 @@ class Core:
 
     def get_titles(self):
         """Return the list of task titles"""
-        return self.titles
+        return self._titles
 
     def get_deadlines(self):
         """Return the list of task deadlines"""
-        return self.deadlines
+        return self._deadlines
 
     def add_task(self, title, deadline):
         """Add a new task"""
-        file = open(self.filename, 'a')
+        file = open(self._filename, 'a')
         task_writer = csv.writer(file, 'nero')
 
         task_writer.writerow([title, deadline])
-        self.titles.append(title)
-        self.deadlines.append(deadline)
+        self._titles.append(title)
+        self._deadlines.append(deadline)
 
         file.close()
 
     def remove_task(self, idx):
         """Remove a task by the index"""
-        file = open(self.filename, 'w')
+        file = open(self._filename, 'w')
         task_writer = csv.writer(file, 'nero')
 
-        del self.titles[idx]
-        del self.deadlines[idx]
+        del self._titles[idx]
+        del self._deadlines[idx]
 
-        task_writer.writerows(zip(self.titles, self.deadlines))
+        task_writer.writerows(zip(self._titles, self._deadlines))
 
         file.close()
